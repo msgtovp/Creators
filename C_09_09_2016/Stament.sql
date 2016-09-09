3feebeee1a5cb178da2fd9f34672fe41c25012e4
msgtovp <msgtovp@gmail.com>
2016-09-09 10:26:47 +0530

    Update Stament.sql

--- C_09_09_2016/Stament.sql
+++ C_09_09_2016/Stament.sql
@@ -4,4 +4,4 @@ use AddressBook;
 
 create table contact_info(identity int primary key auto_increment, firstName varchar(256) not null, lastName varchar(128) default '', phoneNumber varchar(20) default '', mobileNumber varchar(20) not null, emailIdentity varchar(256) default '', website varchar(256) default '', profilePicture mediumblob default '');
 
-insert into contact_info(firstName, lastName, phoneNumber, mobileNumber, emailIdentity, website, profilePicture)  
+insert into contact_info(firstName, lastName, phoneNumber, mobileNumber, emailIdentity, website, profilePicture) values('vijay', '', '', '9698731155', 'msgtovp@gmail.com', 'http://www.microcript.co.in')
