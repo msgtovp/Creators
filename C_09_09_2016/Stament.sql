4ef715576132b5dd157da052d61a681e8d7b4786
msgtovp <msgtovp@gmail.com>
2016-09-09 10:21:27 +0530

    Update Stament.sql

--- C_09_09_2016/Stament.sql
+++ C_09_09_2016/Stament.sql
@@ -2,4 +2,4 @@ create database AddressBook;
 
 use AddressBook;
 
-create table contact_info(identity int primary key auto_increment, firstName varchar(256) not null, lastName varchar(128) default '', phoneNumber varchar(20) default '', mobileNumber varchar(20), emailIdentity varchar(256) default '', website varchar(256) default '', profilePicture mediumblob default '');
+create table contact_info(identity int primary key auto_increment, firstName varchar(256) not null, lastName varchar(128) default '', phoneNumber varchar(20) default '', mobileNumber varchar(20) not null, emailIdentity varchar(256) default '', website varchar(256) default '', profilePicture mediumblob default '');
