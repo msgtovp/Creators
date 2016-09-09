be68d89e0b03da80726857ba9f9e018e656b31bd
msgtovp <msgtovp@gmail.com>
2016-09-09 10:25:00 +0530

    Update Stament.sql

--- C_09_09_2016/Stament.sql
+++ C_09_09_2016/Stament.sql
@@ -3,3 +3,5 @@ create database AddressBook;
 use AddressBook;
 
 create table contact_info(identity int primary key auto_increment, firstName varchar(256) not null, lastName varchar(128) default '', phoneNumber varchar(20) default '', mobileNumber varchar(20) not null, emailIdentity varchar(256) default '', website varchar(256) default '', profilePicture mediumblob default '');
+
+insert into contact_info(firstName, lastName, phoneNumber, mobileNumber, emailIdentity, website, profilePicture)  
