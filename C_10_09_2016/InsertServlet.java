53c2a30ad8050a0666775dda2d5180475c106d87
msgtovp <msgtovp@gmail.com>
2016-09-10 11:36:26 +0530

    Create InsertServlet.java

---
+++ C_10_09_2016/InsertServlet.java
@@ -0,0 +1,56 @@
+package com.addressbook.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InsertServlet
+ */
+@WebServlet("/insertuser")
+public class InsertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try(PrintWriter out = response.getWriter()) {
+			String user = request.getParameter("username");
+			String pass = request.getParameter("password");
+			try {
+				response.setContentType("text/html");
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users","root","JustPass");
+				Statement stmt = conn.createStatement();
+				int count = stmt.executeUpdate("insert into login_users values(null,'"+user+"','"+pass+"')");
+				if(count > 0) {
+					out.println("Successfully inserted");
+					out.println("<a href=index.html>Add Another</a>");
+				} else {
+					response.sendRedirect("index.html");
+				}
+			} catch(Exception e) { 
+				out.println(e);
+			}
+		}
+	}
+   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
