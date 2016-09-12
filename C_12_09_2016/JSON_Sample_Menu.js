f30b0de04acd1c1b80aff19ceccb114eace405b9
msgtovp <msgtovp@gmail.com>
2016-09-12 14:53:17 +0530

    Create JSON_Sample_Menu.js

---
+++ C_12_09_2016/JSON_Sample_Menu.js
@@ -0,0 +1,32 @@
+{
+	"menuitems": [{
+		"id": "file",
+		"Name": "File",
+		"shorcut": "Alt+F",
+		"menuitems": [{
+			"id": "newt",
+			"Name": "New Tab",
+			"shorcut": "Ctrl+T"
+		}, {
+			"id": "neww",
+			"Name": "New Window",
+			"shorcut": "Ctrl+N"
+		}, {
+			"id": "newpw",
+			"Name": "New Private Window",
+			"shorcut": "Ctrl+Shift+P"
+		}, {
+			"id": "open",
+			"Name": "Open File",
+			"shorcut": "Ctrl+O"
+		}, {
+			"id": "closet",
+			"Name": "Close Tab",
+			"shorcut": "Ctrl+W"
+		}, {
+			"id": "closew",
+			"Name": "Close Window",
+			"shorcut": "Ctrl+Shift+W"
+		}]
+	}]
+}
